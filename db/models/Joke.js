30b2d03a540f6166218063d976dfcc0e2fb128fe
Valerija Pintschuk <valerija.pintschuk@posteo.de>
2023-07-27 15:55:25 +0200

    setup for dependencies with mapbox

---
+++ db/models/Joke.js
@@ -0,0 +1,12 @@
+// db/models/Joke.js
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const jokeSchema = new Schema({
+  joke: { type: String, required: true },
+});
+
+const Joke = mongoose.models.Joke || mongoose.model("Joke", jokeSchema);
+
+export default Joke;
